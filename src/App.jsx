78e2c2be4921fc477a1e347a310d78066c9ea81f
Marshal Ram <84084013+marshaljr@users.noreply.github.com>
2026-06-02 23:36:40 +0545

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import LoadMoreData from "./components/load-more-data";
 // import ImageSlider from "./components/image-slider";
 // import StarRating from "./components/star-rating";
 // import Accordian from "./components/accordian/index";
@@ -9,6 +10,7 @@ function App() {
       {/* <Accordian /> */}
       {/* <StarRating noOfStars={8} /> */}
       {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={"10"} /> */}
+      <LoadMoreData />
     </div>
   );
 }
