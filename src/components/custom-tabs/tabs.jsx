87750d334bb78eb9b90a6962a0b66f8e5fe05365
Marshal Ram <84084013+marshaljr@users.noreply.github.com>
2026-06-10 23:26:47 +0545

    refactor: tabs-content

---
+++ src/components/custom-tabs/tabs.jsx
@@ -0,0 +1,14 @@
+export default function Tabs({ tabsContent }) {
+  return (
+    <div className="wrapper">
+      <div className="heading">
+        {tabsContent.map((tabItem) => (
+          <div key={tabItem.label}>
+            <span>{tabItem.label}</span>
+          </div>
+        ))}
+        <div className="content"></div>
+      </div>
+    </div>
+  );
+}
